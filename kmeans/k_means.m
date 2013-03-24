b95839f5fdad3cbb9700b97536ef4a2a8e6327b5
_gjain <gjain@computer.(none)>
2013-03-24 13:47:23 +0100

    Added first version of k-means

---
+++ kmeans/k_means.m
@@ -0,0 +1,75 @@
+function [z, U, score] = k_means(data, nClusters, varargin)
+% K_MEANS   k-means clustering
+% 
+%   [z, U, score] = k_means(data, nClusters, varargin)
+%   Input:
+%       data        Data matrix, columns are observations
+%       nClusters   Number of clusters
+%
+%   Output:
+%       z           Cluster label vector
+%       U           Matrix, columns are estimated cluster means
+%       score       Negative log-likelihood of the estimated solution
+%                   given the data.
+%
+%   Additional parameters through varargin
+%       threshold   Stop if change of log-likelihood smaller than threshold,
+%                   the default is 1e-4.
+%       maxIter     Maximum number of iterations, the default is 100.
+%       means       Initialization of the cluster centroids.
+
+
+% initializations
+threshold = 1e-4;
+maxIter = 100;
+[nItems, nExamples] = size(data);
+Z = zeros(nClusters, nExamples);
+score = realmax;
+change = threshold+1;
+iterations = 0;
+
+% initialization of the class means
+indices = randperm(nExamples);
+U = data(:,indices(1:nClusters));
+
+% parse varargin
+for k=1:2:length(varargin)
+    switch lower(varargin{k})
+    case 'threshold'
+        threshold = varargin{k+1};
+    case 'maxiter'
+        maxIter = varargin{k+1};
+    case 'means'
+        U = varargin{k+1};
+    otherwise
+        error(['Unknown parameter ''' varargin{k} '''.']);
+    end
+end
+
+% k-Means algorithm
+id_col = 1:nExamples;
+z_size = [nClusters, nExamples];
+while (change>threshold && iterations < maxIter),
+    iterations = iterations+1;
+    disp(sprintf('Iterations = %d  Change = %0.5g.', iterations, change));
+
+    % Assignment step: estimate class indices
+    for k = 1:nClusters
+        Z(k,:) = sum(((data - repmat(U(:,k), 1, nExamples)).^2), 1);
+    end
+    
+    [bar, id_row] = min(Z);
+    Z = Z.*0;
+    Z(sub2ind(z_size, id_row, id_col)) = 1;
+    
+    % Update step: estimate means
+    U = ((Z*(data'))./(repmat(sum(Z, 2), 1, nItems)))';
+    
+    % estimate change
+    score_old = score;
+    score = sum(sum((data - U*Z).^2, 1));
+    change = score_old - score;
+end
+
+% convert assignments to vector representation
+[foo, z] = max(Z, [], 1);
--- svd/PredictMissingValues.m
+++ svd/PredictMissingValues.m
@@ -6,6 +6,7 @@ function X_pred = PredictMissingValues(X, nil)
 X_pred = X;
 % X_pred(X_pred == nil) = 1;
 
+% Replace nil by row-average
 not_nil = (X_pred ~= nil);
 not_nil_sum_row = sum(not_nil');
 nil_m_idx = (X_pred == nil);
@@ -18,4 +19,24 @@ avg = avg./not_nil_sum_row;
 for i = 1:size(X,1),
     X_pred(i, nil_m_idx(i, :)) = avg(i);
 end
+
+% Proceed with SVD
+[U, D, V] = svd(X);
+D_root = sqrt(D);
+%U = U*D_root;
+%V = D_root*V;
+
+k = 100;
+D(k+1:end,1:end) = 0;
+
+Pred = U*D;
+Pred = Pred*(V');
+%size(Pred)
+%size(nil_m_idx)
+Pred = Pred.*nil_m_idx;     % Predictions correspond to only the null values
+X_pred = X_pred.*not_nil;
+
+X_pred = X_pred + Pred;
+
+
 
